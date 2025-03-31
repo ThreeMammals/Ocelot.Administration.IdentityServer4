e3884cb817b2998786a7c64fd50ff3f31e19f7f9
Raman Maksimchuk <dotnet044@gmail.com>
2025-03-31 12:39:06 +0300

    Unit testing project

---
+++ unit/UnitTest.cs
@@ -0,0 +1,7 @@
+﻿namespace Ocelot.Administration.IdentityServer4.UnitTests;
+
+public class UnitTest
+{
+    protected readonly Guid _testId = Guid.NewGuid();
+    protected string TestID { get => _testId.ToString("N"); }
+}
